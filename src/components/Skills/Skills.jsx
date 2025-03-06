3ccf63044f86b36a04f23329a29991082a7d4fe9
Vladimir <adda37@yandex.ru>
2025-03-06 20:09:47 +0300

    commit for react

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -9,7 +9,7 @@ export default function Skills() {
                 <H2 titleText={'Skills'}/>
                 <p className='skills-text'>I work in such programs as</p>
                 <div className="skills-all">
-                    <Skill name='Adobe Photoshop' img={'/img/PS.svg'} numofskills={4} />
+                    <Skill name='Adobe Photoshop' img={'./novikreact/img/PS.svg'} numofskills={4} />
                     <Skill name='Adobe Illustrator' img={'/img/AI.svg'} numofskills={3} />
                     <Skill name='Adobe After Effects' img={'/img/AE.svg'} numofskills={4} />
                     <Skill name='Figma' img={'/img/Figma.svg'} numofskills={4} />
